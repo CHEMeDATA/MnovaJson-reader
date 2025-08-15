796421bfccffcebc1d8f65b9647a58896cf1b686
djeanner <djeanner@users.noreply.github.com>
2025-08-15 12:26:09 +0200

    improved demo NMR with noise and peaks

--- install.zsh
+++ install.zsh
@@ -2,16 +2,25 @@ mkdir -p external
 curl -o -L external/nmrSpectrum.js https://raw.githubusercontent.com/CHEMeDATA/NMRspectrum-viewer/main/src/nmrSpectrum.js
 curl -o -L external/graphBase.js https://raw.githubusercontent.com/CHEMeDATA/NMRspectrum-viewer/main/src/graphBase.js
 
+echo 
+echo "finished curls commands"
+echo 
+
+echo "create external/nmrSpectrumObject.js"
 cat src/objFolder/nmrSpectrumObject.js \
 | sed '/\/\/ AUTOMATIC IMPORT INSERTION WILL BE MADE HERE/r src/objFolder/exportMnovaImport.js' \
 | sed '/\/\/ AUTOMATIC METHOD INSERTION WILL BE MADE HERE/r src/objFolder/exportMnovaMethod.js' \
 > external/nmrSpectrumObject.js
 
+echo "create external/jGraphObject.js"
 cat src/objFolder/jGraphObject.js \
 | sed '/\/\/ AUTOMATIC IMPORT INSERTION WILL BE MADE HERE/r src/objFolder/exportMnovaImport.js' \
 | sed '/\/\/ AUTOMATIC METHOD INSERTION WILL BE MADE HERE/r src/objFolder/exportMnovaMethod.js' \
 > external/jGraphObject.js
 
-cp src/objFolder/mnovaJsonReader.js external/mnovaJsonReader.js 
+echo "copy mnovaJsonReader.js"
+cp src/mnovaJsonReader.js external/mnovaJsonReader.js 
+
+echo "copy ObjectBase.js"
 cp src/objFolder/ObjectBase.js external/ObjectBase.js 
 
--- src/objFolder/nmrSpectrumObject.js
+++ src/objFolder/nmrSpectrumObject.js
@@ -1,4 +1,3 @@
-
 // AUTOMATIC IMPORT INSERTION WILL BE MADE HERE
 
 import { ObjectBase } from "./ObjectBase.js";
@@ -10,21 +9,63 @@ export class NMRspectrumObject extends ObjectBase {
 		this.verbose = 0;
 	}
 
-	_handleLoadDemoData(numberOfSpectra) {
-		this._loadDemoData(numberOfSpectra);
+	_handleLoadDemoData() {
+		this._loadDemoData();
 	}
 
-	_loadDemoData(numberOfSpectra) {
-		const values = Array.from({ length: 16000 }, (_, i) => {
-			return (i + Math.random() * 2000.0 - 1000.0) / 1000.0;
-		});
+	_loadDemoData(
+		spectralData = {
+			pointNumber: 32768,
+			larmor: 500,
+			firstPoint: 11,
+			lastPoint: -1.0,
+			noiseLevel: 1000,
+		},
+		arrayLorentzian = {
+			centers: [7.27, 5.0, 0.0],
+			widthsInHz: [0.7, 0.7, 0.7],
+			amplitudes: [100, 1000, 100],
+		}
+	) {
+		const values = [];
+		const mean = 0;
+
+		for (let i = 0; i < spectralData.pointNumber; i++) {
+			let u = 0,
+				v = 0;
+			while (u === 0) u = Math.random();
+			while (v === 0) v = Math.random();
+			const amplitude = Math.sqrt(-2.0 * Math.log(u));
+			const realPart = amplitude * Math.cos(2.0 * Math.PI * v);
+			//const complexPart = amplitude * Math.sin(2.0 * Math.PI * v);
+			values.push(realPart * spectralData.noiseLevel + mean);
+		}
+		function lorentzian(arrayLorentzian, larmor, x, i) {
+			const dx = x - arrayLorentzian.centers[i];
+			const gamma = arrayLorentzian.widthsInHz[i] / spectralData.larmor;
+
+			return (
+				arrayLorentzian.amplitudes[i] /
+				(Math.PI * gamma * (1 + (dx / gamma) ** 2))
+			);
+		}
+
+		const increment =
+			(spectralData.lastPoint - spectralData.firstPoint) /
+			(spectralData.pointNumber - 1);
+		for (let i = 0; i < arrayLorentzian.centers.length; i++) {
+			for (let index = 0; index < values.length; index++) {
+				const x = spectralData.firstPoint + increment * index;
+				values[index] += lorentzian(arrayLorentzian, spectralData.larmor, x, i);
+			}
+		}
+
 		this.data = {
 			values: values,
-			firstPoint: 11.0,
-			lastPoint: -1.0,
+			firstPoint: spectralData.firstPoint,
+			lastPoint: spectralData.lastPoint,
 		};
 	}
 
-// AUTOMATIC METHOD INSERTION WILL BE MADE HERE
-
+	// AUTOMATIC METHOD INSERTION WILL BE MADE HERE
 }
