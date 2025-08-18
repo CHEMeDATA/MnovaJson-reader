06e178bd7195d12ee46360360c8569cd3cb1b9b7
djeanner <djeanner@users.noreply.github.com>
2025-08-18 12:04:23 +0200

    update after moving additions of methods in the nmr-objects repository

--- install.zsh
+++ install.zsh
@@ -3,21 +3,11 @@ mkdir -p external
 curl -o -L external/nmrSpectrum.js https://raw.githubusercontent.com/CHEMeDATA/NMRspectrum-viewer/main/src/nmrSpectrum.js
 curl -o -L external/graphBase.js https://raw.githubusercontent.com/CHEMeDATA/NMRspectrum-viewer/main/src/graphBase.js
 
+curl -o -L external/jGraphObject.js https://raw.githubusercontent.com/CHEMeDATA/nmr-objects/dist/jGraphObject.js
+curl -o -L external/nmrSpectrumObject.js https://raw.githubusercontent.com/CHEMeDATA/nmr-objects/main/dist/nmrSpectrumObject.js
 echo 
 echo "finished curls commands"
 echo 
-
-echo "create external/nmrSpectrumObject.js"
-cat src/objFolder/nmrSpectrumObject.js \
-| sed '/\/\/ AUTOMATIC IMPORT INSERTION WILL BE MADE HERE/r src/importStatements.js' \
-| sed '/\/\/ AUTOMATIC METHOD INSERTION WILL BE MADE HERE/r src/importMethod.js' \
-> external/nmrSpectrumObject.js
-
-echo "create external/jGraphObject.js"
-cat src/objFolder/jGraphObject.js \
-| sed '/\/\/ AUTOMATIC IMPORT INSERTION WILL BE MADE HERE/r src/importStatements.js' \
-| sed '/\/\/ AUTOMATIC METHOD INSERTION WILL BE MADE HERE/r src/importMethod.js' \
-> external/jGraphObject.js
 
 echo "copy mnovaJsonReader.js"
 cp src/mnovaJsonReader.js external/mnovaJsonReader.js 
