b2d9d9d48623cb8709e12586679660cf5c8153e7
djeanner <djeanner@users.noreply.github.com>
2025-09-03 15:28:36 +0200

    update

--- src/exportMethod.js
+++ src/exportMethod.js
@@ -1,5 +1,14 @@
 //  export method // Should not minimize
 	export_Editordjeanner_Version1_SourceMnovaJson_IDnone(param, dataInput) {
+
+		if (!dataInput.origin) {
+			console.error("No origin data in dataInput for import", dataInput);
+			this.data = {};
+			// process.exit(1);
+		}
+		this.origin = dataInput.origin;
+		this.conversionParameters = param;
+
 		if (param.requestedField) {
 			if (param.requestedField === "first") {
 				return {dummy_data : 1};
